517eb643bc8f6b824be0b6821c78e707c7782da6
Draxent <fconte90@gmail.com>
2025-06-23 23:58:31 +0200

    chore: update dependencies
    Updated Flutter SDK requirement to ^3.8.1 and bumped several dependencies, including flutter_bloc to ^9.1.1

--- example/lib/src/router.dart
+++ example/lib/src/router.dart
@@ -17,31 +17,22 @@ class AppRouter {
   AppRouter()
     : router = GoRouter(
         routes: [
-          GoRoute(
-            path: HomePage.path,
-            name: HomePage.name,
-            builder: (_, __) => inject<HomePage>(),
-          ),
-          GoRoute(
-            path: BasicPage.path,
-            name: BasicPage.name,
-            builder: (_, __) => BasicPage(),
-          ),
+          GoRoute(path: HomePage.path, name: HomePage.name, builder: (_, _) => inject<HomePage>()),
+          GoRoute(path: BasicPage.path, name: BasicPage.name, builder: (_, _) => BasicPage()),
           GoRoute(
             path: CounterPage.path,
             name: CounterPage.name,
-            builder: (_, __) => CounterPage(model: const CounterModel()),
+            builder: (_, _) => CounterPage(model: const CounterModel()),
           ),
           GoRoute(
             path: RestaurantsPage.path,
             name: RestaurantsPage.name,
-            builder:
-                (_, __) => RestaurantsPage(model: const RestaurantsModel()),
+            builder: (_, _) => RestaurantsPage(model: const RestaurantsModel()),
           ),
           GoRoute(
             path: IntegratedPage.path,
             name: IntegratedPage.name,
-            builder: (_, __) => IntegratedPage(model: const IntegratedModel()),
+            builder: (_, _) => IntegratedPage(model: const IntegratedModel()),
           ),
         ],
       );
